2b8d0e91bd2cdcdbf07c539d427d384b8e4e54f6
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-04 15:46:47 +0200

    [UPD]Added content in index.php

--- index.php
+++ index.php
@@ -7,9 +7,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="./quest.php" method="GET">
+                    <div class="mb-3">
+                        <label class="form-label">Inserisci qui il testo</label>
+                        <textarea type="text" class="form-control" name="text"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Inserisci qui la parola da censurare</label>
+                        <input type="word" class="form-control" name="word">
+                    </div>
+                    <button type="submit" class="btn btn-primary bg-success">Invio</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
